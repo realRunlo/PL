efecd3d4e36cf8ccf2870b04de514602c335f93f
Tiago Silva <a93277@alunos.uminho.pt>
2022-03-22 12:03:51 +0000

    aspas e , no header (nao suporta \n no header)

--- projeto.py
+++ projeto.py
@@ -11,23 +11,30 @@ def str_to_sum (str):
 def str_to_media (str):
     return statistics.mean([int(s) for s in str.split(',')])
 
+def increment_str(str):
+    if(ord(str[0]) < 122):
+        str = chr(ord(str[0])+1) + str[1:]
+    else:
+        str = "a" + str[0:]
+    return str
+
 tokens = ["LISTA","SEPARADOR","NOME"]
 
 def t_LISTA(t):
-    r'(([^,\n]+)|("[^"]+"))\{\d+(,\d+)?\}(::\w+)?'
+    r'(([^",\n]+)|("[^"]+"))\{\d+(,\d+)?\}(::\w+)?'
     q_min = 0
     q_max = 0
-    m = re.match(r'(?P<nome>([^,\n]+)|(\"[^"]+"))\{(?P<q_min>\d)(,(?P<q_max>\d))?\}(?P<funcao>::\w+)?',t.value)
+    m = re.match(r'(?P<nome>([^",\n]+)|(\"[^"]+"))\{(?P<q_min>\d)(,(?P<q_max>\d))?\}(?P<funcao>::\w+)?',t.value)
     if(m.group("nome")):
         t.value = m.group("nome")
     if(m.group("funcao")):
-        lexer.funcoes.append((t.value,m.group("funcao")))
-    else: lexer.funcoes.append((t.value,"normal"))
+        lexer.content.append((lexer.i,m.group("funcao"),t.value))
+    else: lexer.content.append((lexer.i,"::",t.value))
     if(m.group("q_min")):
         q_min = int(m.group("q_min"))
     if(m.group("q_max")):
         q_max = int(m.group("q_max"))
-    lexer.exp = lexer.exp + r'(?P<' + t.value+ r'>'
+    lexer.exp = lexer.exp + r'(?P<' + lexer.i + r'>'
     for i in range(q_min-1):
         lexer.exp = lexer.exp + r'\d+,'
     lexer.exp = lexer.exp + r'\d+'
@@ -35,11 +42,14 @@ def t_LISTA(t):
         lexer.exp = lexer.exp + r',(\d+)?'
     lexer.separator = False
     lexer.exp = lexer.exp + r')'
+    lexer.i = increment_str(lexer.i)
     return t
 
 def t_NOME(t):
-    r'([^,\n]+)|("[^"]+")'
-    lexer.exp = lexer.exp + r'(?P<' + t.value + r'>([^,\n]+)|("[^"]+"))'
+    r'([^",\n]+)|("[^"]+")'
+    lexer.exp = lexer.exp + r'(?P<' + lexer.i + r'>([^",\n]+)|("[^"]+"))'
+    lexer.content.append((lexer.i,"",t.value))
+    lexer.i = increment_str(lexer.i)
     lexer.separator = False
     return t
 
@@ -56,10 +66,10 @@ def t_ANY_error(t):
     print("Illegal Character")
 
 lexer = lex.lex()
-lexer.funcoes = []
+lexer.content = []
 lexer.exp = r''
 lexer.separator = False
-lexer.i = 0
+lexer.i = "a"
 
 file = open("texto.csv","r",encoding="utf-8")
 
@@ -102,15 +112,22 @@ print(lexer.exp)
 mos = exp.finditer(content)
 for mo in mos:
     dict = mo.groupdict()
-    for tuplo in lexer.funcoes:
+    for tuplo in lexer.content:
+            nomeProv = tuplo[0]
             funcao = tuplo[1]
-            nome = tuplo[0]
-            if(funcao == "normal"):
-                dict[nome] = str_to_list(dict[nome])
+            nomeCorr = tuplo[2]
+            if(funcao == "::"):
+                dict[nomeProv] = str_to_list(dict[nomeProv])
             elif(funcao == "::sum"):
-                dict[nome] = str_to_sum(dict[nome])
+                dict[nomeProv] = str_to_sum(dict[nomeProv])
             elif(funcao == "::media"):
-                dict[nome] = str_to_media(dict[nome])
+                dict[nomeProv] = str_to_media(dict[nomeProv])
+            dict[nomeCorr] = dict.pop(nomeProv)
             i+=1
     print(dict)
+file.close()
+
+file = open("texto.json","w+",encoding="utf-8")
+file.write("[\n")
+file.write("\n]")
 file.close()
